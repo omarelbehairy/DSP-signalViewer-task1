af8526ec1be9429b00353ccb6085cdf398daf983
EizaldinTarik <Eizaldint@gmail.com>
2023-02-22 19:33:50 +0200

    remake + Choose file + static view

--- app.py
+++ app.py
@@ -4,15 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # read values from text file
-    with open('ECG_T.txt', 'r') as f:
-        lines = f.readlines()
-
-    # convert to list of tuples
-    data = [tuple(map(float, line.strip().split('\t'))) for line in lines]
-
-    # render HTML template with data
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
